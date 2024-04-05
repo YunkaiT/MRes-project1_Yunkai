e5a09f7f69adae941b86b93f4506bea0c6d1e5bf
YunkaiT <151536283+YunkaiT@users.noreply.github.com>
2024-04-05 02:12:01 +0100

    Update Total Pipeline.sh

--- Total Pipeline.sh
+++ Total Pipeline.sh
@@ -1,29 +1,23 @@
-#!/bin/bash
+# These pipeline was built for running in my environment, minor adjustment should be made when running in a different environment
 
-#PBS -l select=1:ncpus=150:mem=500gb
+
+# I run in PBS job submission system, I applied for the following amount of resources to process 10-20 WGS data (in BAM format) each time.
+#!/bin/bash
+#PBS -l select=1:ncpus=220:mem=800gb
 #PBS -l walltime=20:00:00
 #PBS -N rscript_job
+
+
+
+# My working environment was myenv123
 module load anaconda3/personal
 source activate myenv123
 
 
-
-
+# But if you want to use a new envrionment, for example, called "Yunkai", please download the following packages.
 
 # Download samtools in the environment
 conda install -n Yunkai bioconda::samtools -y
-# Create a file called "BAM" to save the original 159 files
-mkdir BAM
-# Download the data from HPC to the local "BAM" folder
-## Download data of patients 1 to 11
-
-
-# Download QDNAseq file "QDNAseq_from_bam_chrX.R", shallowHRD file "312shallowHRD_hg19_1.13_QDNAseq_chrX.R", and "./cytoband_adapted_hg19.csv" file
-BAM_BASE_DIR="./"
-QDNA_SEQ_SCRIPT="./QDNAseq_from_bam_chrX.R"
-SHALLOW_HRD_SCRIPT="./312shallowHRD_hg19_1.13_QDNAseq_chrX.R"
-CYTOBAND_FILE="./cytoband_adapted_hg19.csv"
-
 
 # Download the packages for QDNAseq
 conda install -n Yunkai -y bioconda::bioconductor-biobase
@@ -42,31 +36,25 @@ conda install -n Yunkai -y conda-forge::r-desctools
 conda install -n Yunkai -y conda-forge::r-ggrepel
 ## Download "devtools" package for downloading the "ks" package with the version of 1.14.2
 conda install -n Yunkai -y conda-forge::r-devtools
-## Download "ks" with the version of 1.14.2
+# Download "ks" with the version of 1.14.2
 R -e "devtools::install_version('ks', version = '1.14.2')"
 
 
-
-
-
-cd /rds/general/ephemeral/user/yt1823/ephemeral/pipeline1415
-mkdir BAM
-
-
-##copy files to BAM
-cp /rds/general/project/pc_multiomics_project1/live/MRes/Yunkai/Data/all22/{14_T16-156_FT13.dups_marked.sorted.bam,14_T16-156_FT16.dups_marked.sorted.bam,14_T16-156_FT1.dups_marked.sorted.bam,14_T16-156_FT3.dups_marked.sorted.bam,14_T16-156_FT8.dups_marked.sorted.bam,14_T16-156_SN16-232_BC.dups_marked.sorted.bam,15_T16-186_BC1.dups_marked.sorted.bam,15_T16-186_FT1.dups_marked.sorted.bam,15_T16-186_FT2.dups_marked.sorted.bam,15_T16-186_FT5.dups_marked.sorted.bam,15_T16-186_FT6.dups_marked.sorted.bam,15_T16-186_FT8.dups_marked.sorted.bam} /rds/general/ephemeral/user/yt1823/ephemeral/pipeline1415/BAM/
-
-
-
-
-
-
-
-# Download QDNAseq file "QDNAseq_from_bam_chrX.R", shallowHRD file "312shallowHRD_hg19_1.13_QDNAseq_chrX.R", and "./cytoband_adapted_hg19.csv" file
+# Put these files in your working directory: QDNAseq file "QDNAseq_from_bam_chrX.R", shallowHRD file "312shallowHRD_hg19_1.13_QDNAseq_chrX.R", and "./cytoband_adapted_hg19.csv" file 
 BAM_BASE_DIR="./"
 QDNA_SEQ_SCRIPT="./QDNAseq_from_bam_chrX.R"
 SHALLOW_HRD_SCRIPT="./312shallowHRD_hg19_1.13_QDNAseq_chrX.R"
 CYTOBAND_FILE="./cytoband_adapted_hg19.csv"
+# Rememver also put the name list of the WGS data that you are working on. Here I put my list called "bam_file_names.txt"
+
+
+# Create a file called "BAM" to save WGS BAM files. In this study, original 159 files were saved and used.
+mkdir BAM
+
+# Download the data from HPC to the local "BAM" folder. Here we put all 12 files from patient 6 and patient 7. You can adjust the paths as you want to test different WGS data.
+cp /rds/general/project/pc_multiomics_project1/live/MRes/Yunkai/Data/all22/{6_T14-125_157_BC.dups_marked.sorted.bam,6_T14-125_62_FT6.dups_marked.sorted.bam,6_T14-125_63_FT7.dups_marked.sorted.bam,6_T14-125_64_FT8.dups_marked.sorted.bam,6_T14-125_65_FT9.dups_marked.sorted.bam,7_T14-137_158_BC.dups_marked.sorted.bam,7_T14-137_67_FT2.dups_marked.sorted.bam,7_T14-137_68_FT3.dups_marked.sorted.bam,7_T14-137_69_FT5.dups_marked.sorted.bam,7_T14-137_70_FT7.dups_marked.sorted.bam,7_T14-137_71_FT9.dups_marked.sorted.bam,7_T17-067_140_A.dups_marked.sorted.bam,7_T17-067_141_B.dups_marked.sorted.bam,7_T17-067_142_C1.dups_marked.sorted.bam,7_T17-067_143_D.dups_marked.sorted.bam,7_T17-067_144_E.dups_marked.sorted.bam,7_T17-067_145_F.dups_marked.sorted.bam,7_T17-067_146_G.dups_marked.sorted.bam} /rds/general/ephemeral/user/yt1823/ephemeral/pipeline67/BAM/
+
+
 
 
 # Downsampling
@@ -87,13 +75,13 @@ downsampling(){
 }
 export -f downsampling
 
-# Downsampling 1-12 BAM files in the list
-samples=$(sed -n '26,37p' bam_file_names.txt)
+# Downsampling 124th-141st BAM files from the "bam_file_names.txt"
+samples=$(sed -n '124,141p' bam_file_names.txt)
 
 
 
-# Conduct parallel downsampling, the number of parallel processes can change, for exaple, 24 here
-parallel -j 144 downsampling {1} {2} {3} ::: $samples ::: 0.6667 0.3333 0.1667 0.0333 0.0267 0.02 0.0167 0.0133 0.01 0.0067 0.0033 0.00167 :::+ 20x 10x 5x 1x 0.8x 0.6x 0.5x 0.4x 0.3x 0.2x 0.1x 0.05x
+# Conduct parallel downsampling, the number of the parallel task running together each time can be changed, here I recommed to set the number of j to 11 x the number of WGS file processed, as there are 11 downsampling depths.
+parallel -j 198 downsampling {1} {2} {3} ::: $samples ::: 0.6667 0.3333 0.1667 0.0333 0.0267 0.02 0.0167 0.0133 0.01 0.0067 0.0033 0.00167 :::+ 20x 10x 5x 1x 0.8x 0.6x 0.5x 0.4x 0.3x 0.2x 0.1x 0.05x
 
 
 
@@ -110,8 +98,7 @@ processQDNAseq() {
     Rscript QDNAseq_from_bam_chrX.R "$bamFile" "$outputDir" 50
 }
 
-export -f processQDNAseq    # 导出函数以便parallel可以使用
-
+export -f processQDNAseq   
 # Defining an associative array to map downsampled directories to QDNA output directories
 declare -A downsampleToQDNA=(
     ["BAM"]="30xQDNA"
@@ -132,17 +119,16 @@ declare -A downsampleToQDNA=(
 taskFile="tasks.txt"
 > "$taskFile"  
 
-# 生成任务列表
+# Generate task file
 for downsampleDir in "${!downsampleToQDNA[@]}"; do
-    outputDir="${downsampleToQDNA[$downsampleDir]}"  # 根据关联数组计算输出目录
+    outputDir="${downsampleToQDNA[$downsampleDir]}" 
     for bamFile in ./"$downsampleDir"/*sorted.bam; do
-        # 写入任务到文件：BAM文件路径 和 输出目录
         echo "$bamFile $outputDir" >> "$taskFile"
     done
 done
 
 # Use parallel processing to run QDNAseq
-parallel --colsep ' ' -j 144 processQDNAseq {1} {2} :::: "$taskFile"
+parallel --colsep ' ' -j 198 processQDNAseq {1} {2} :::: "$taskFile"
 
 
 
@@ -190,4 +176,3 @@ for downsampleDir in "${!downsampleToQDNA[@]}"; do
     ##find "$qdnaOutputDir" -type f | parallel -j 8 process_qdna {} "$qdnaOutputDir"
 
 done
-
